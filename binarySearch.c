51c62faee49c6af6b5477af47a3507200f48912f
112534090-netizen <112534090@kanchiuniv.ac.in>
2026-02-13 02:40:41 -0800

    Add files via upload

---
+++ binarySearch.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+ int binarysearch (int arr[],int n,int key)
+{
+		int low=0,high=n-1,mid;
+	while(low<=high)
+	{
+		mid=(low+high)/2;
+		if(arr[mid]==key)
+		return mid;
+		else if (arr[mid]<key)
+		low=mid+1;
+		 else                                 
+		high=mid-1;
+	}
+	return-1;
+}
+int main()
+{
+	int n,key,result;
+	printf("enter number of element:"); 
+	scanf("%d",&n);
+	int arr[n];
+	printf("enter %d sorted element:\n",n);
+	 for(i=0;i<n;i++)
+	scanf("%d",&arr[i]);
+	printf("enter element to search:");
+	scanf("%d",&key);
+	result=binarysearch(arr,n,key);
+	if(result!=-1)
+	printf("element found at imdex %d\n",result);
+	else
+	printf("element not found \n");
+	return 0;
+}
+	
